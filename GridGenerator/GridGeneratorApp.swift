75253434d7d0386dbdb93506c3633372fb76adbb
Illia Senchukov <senchukov02@gmail.com>
2024-02-24 07:03:51 +0200

    Initial Commit

---
+++ GridGenerator/GridGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridGeneratorApp.swift
+//  GridGenerator
+//
+//  Created by Illia Senchukov on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GridGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
